439afc743dd3df84767ddddb0f4a2344d37c2db6
Aaron Hoffman <ajh269@uakron.edu>
2024-05-20 21:56:23 -0400

    Get all files in a given directory recursively

--- src/renderer.ts
+++ src/renderer.ts
@@ -4,3 +4,26 @@
 // nodeIntegration is set to true in webPreferences.
 // Use preload.js to selectively enable features
 // needed in the renderer process.
+
+// Load all scripts from the renderer directory
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+// Recursively get all files from a starting directory
+const getAllFiles = (dir: string): string[] => {
+  const files: string[] = [];
+
+  fs.readdirSync(dir).forEach(file => {
+    const filePath = path.join(dir, file);
+    if (fs.statSync(filePath).isDirectory()) {
+      files.push(...getAllFiles(filePath));
+    } else {
+      files.push(filePath);
+    }
+  });
+  return files;
+}
+
+const allFiles = getAllFiles('renderer');
+console.log(allFiles);
