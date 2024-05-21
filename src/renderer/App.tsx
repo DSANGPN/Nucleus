d8ce9f644e2701d094e0aeac110dbe3afa3af25f
Aaron Hoffman <ajh269@uakron.edu>
2024-05-21 18:16:47 -0400

    Add dygraphs dependency and test imports

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,9 +1,21 @@
 // Driver for displaying something.
 // In general, all components should be in organized directories
+
+import Dygraph from 'dygraphs';
+import * as React from 'react';
+import { DefaultButton } from '@fluentui/react/lib/Button';
+
 function Hello() {
   return <h1>Hello!</h1>;
 }
 
 export default function App() {
-  return <Hello />;
+  console.log(Dygraph);
+
+  return (
+    <>
+      <Hello />
+      <DefaultButton text="Standard Butt" />
+    </>
+  );
 }
