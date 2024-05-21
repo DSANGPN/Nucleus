5db76ec9746715668ac16091f2ed8ee7239e3f25
Aaron Hoffman <ajh269@uakron.edu>
2024-05-21 17:28:27 -0400

    Use ERB

---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+// Driver for displaying something.
+// In general, all components should be in organized directories
+function Hello() {
+  return <h1>Hello!</h1>;
+}
+
+export default function App() {
+  return <Hello />;
+}
