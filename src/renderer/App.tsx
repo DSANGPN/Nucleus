7c72b78853cd0c93566828ba2e7cdb7724157b9a
Aaron Hoffman <ajh269@uakron.edu>
2024-05-21 21:33:45 -0400

    More functionality in App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,17 +2,25 @@
 // In general, all components should be in organized directories
 
 import * as React from 'react';
-import { DefaultButton } from '@fluentui/react/lib/Button';
+import { useState } from 'react';
+import { DefaultButton } from '@fluentui/react';
 
 function Hello() {
   return <h1>Hello!</h1>;
 }
 
 export default function App() {
+  const [num, setNum] = useState(0);
+
+  const handleClick = () => {
+    setNum(num + 1);
+  };
+
   return (
     <>
       <Hello />
-      <DefaultButton text="Standard Butt" />
+      <DefaultButton text="Standard Butt" onClick={handleClick} />
+      <h1>{num}</h1>
     </>
   );
 }
