7c9a29bd2603ddfba08ae6c5c701acd59b91199b
Aaron Hoffman <ajh269@uakron.edu>
2024-05-21 20:58:49 -0400

    Remove dygraphs

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,6 @@
 // Driver for displaying something.
 // In general, all components should be in organized directories
 
-import Dygraph from 'dygraphs';
 import * as React from 'react';
 import { DefaultButton } from '@fluentui/react/lib/Button';
 
@@ -10,8 +9,6 @@ function Hello() {
 }
 
 export default function App() {
-  console.log(Dygraph);
-
   return (
     <>
       <Hello />
