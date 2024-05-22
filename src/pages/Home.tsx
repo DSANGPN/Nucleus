28c1d2a8b3d50b4c69f9f5e96c497120920049c8
Aaron Hoffman <ajh269@uakron.edu>
2024-05-21 21:37:27 -0400

    Extract app into a Home Page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+// Driver for displaying something.
+// In general, all components should be in organized directories
+
+import * as React from 'react';
+import { useState } from 'react';
+import { DefaultButton } from '@fluentui/react';
+
+function Hello() {
+  return <h1>Hello!</h1>;
+}
+
+export default function Home() {
+  const [num, setNum] = useState(0);
+
+  const handleClick = () => {
+    setNum(num + 1);
+  };
+
+  return (
+    <>
+      <Hello />
+      <DefaultButton text="Standard Butt" onClick={handleClick} />
+      <h1>{num}</h1>
+    </>
+  );
+}
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,26 +1,12 @@
 // Driver for displaying something.
 // In general, all components should be in organized directories
 
-import * as React from 'react';
-import { useState } from 'react';
-import { DefaultButton } from '@fluentui/react';
-
-function Hello() {
-  return <h1>Hello!</h1>;
-}
+import Home from '../pages/Home'
 
 export default function App() {
-  const [num, setNum] = useState(0);
-
-  const handleClick = () => {
-    setNum(num + 1);
-  };
-
   return (
     <>
-      <Hello />
-      <DefaultButton text="Standard Butt" onClick={handleClick} />
-      <h1>{num}</h1>
+      <Home/>
     </>
   );
 }
