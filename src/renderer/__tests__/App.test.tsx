498ff50013aef8aff6d408d01585145497658290
Aaron Hoffman <ajh269@uakron.edu>
2024-05-25 16:12:27 -0400

    Add contextBridge mock for tests

--- src/renderer/__tests__/App.test.tsx
+++ src/renderer/__tests__/App.test.tsx
@@ -2,6 +2,20 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import App from '../App';
 
+// This is how it is defined in the context bridge in my code. Modify this as required
+const mockElectron = {
+  ipcRenderer: {
+    sendMessage: jest.fn(),
+    on: jest.fn(),
+    once: jest.fn(),
+    removeEventListener: jest.fn(),
+  },
+};
+
+beforeAll(() => {
+  global.window.electron = mockElectron;
+});
+
 describe('App', () => {
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
